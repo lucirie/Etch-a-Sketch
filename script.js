4e7061c7b85dedeacbeea99feaa7cb6202a38ebf
lucirie <lucirieko@gmail.com>
2025-05-16 18:20:42 +0300

    Random color upon hover Using math.random to generate a random set of 8 digits for the hex color code

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const container = document.querySelector(".container");
 function add_divs(size) {
     for (i = 0; i < size; i++) {
         row = document.createElement("div");
-        row.classList.add("row")
+        row.classList.add("row");
         for (j = 0; j < size; j++) {
             box_number = j + size * i;
             box = document.createElement("div");
-            box.classList.add("box")
-            box.classList.add(box_number)
+            box.classList.add("box");
+            box.classList.add(box_number);
             row.appendChild(box);
         }
-        container.appendChild(row)
+        container.appendChild(row);
     }
 }
 
@@ -26,7 +26,9 @@ function detectHover(divCount) {
     boxes = document.querySelectorAll(".box");
     for (i = 0; i < (divCount ** 2); i++) {
         boxes[i].addEventListener('mouseover', (e) => {
-            e.currentTarget.style.backgroundColor = "black";
+            let bg_colour = Math.floor(Math.random() * 16777216).toString(16);
+            bg_colour = "#" + ("000000" + bg_colour).slice(-6);
+            e.currentTarget.style.backgroundColor = bg_colour;
         })
     }
 }
